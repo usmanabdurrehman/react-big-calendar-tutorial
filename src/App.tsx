b01fb58edfdf0d24947995f097c029d1daa720d8
Usman Abdur Rehman <saidskit@outlook.com>
2023-04-17 08:17:54 -0400

    Clean Master

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import { useState } from "react";
-import {
-  BasicCalendar,
-  ControlCalendar,
-  CustomizingCalendar,
-  AdvancedCalendar,
-} from "./Components";
 
 function App() {
-  return (
-    <div style={{ height: "95vh" }}>
-      {/* <BasicCalendar /> */}
-      {/* <ControlCalendar /> */}
-      {/* <CustomizingCalendar /> */}
-      <AdvancedCalendar />
-    </div>
-  );
+  return <div style={{ height: "95vh" }}>React Big Calendar Tutorial</div>;
 }
 
 export default App;
